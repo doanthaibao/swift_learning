1f6233ff5378c623195228613fd62068755d5c3a
Bao Doan <bao.doan@sap.com>
2018-09-19 13:11:58 +0700

    Adding first project

---
+++ Singleton/Singleton/Singleton.h
@@ -0,0 +1,19 @@
+//
+//  Singleton.h
+//  Singleton
+//
+//  Created by Bao Doan on 9/19/18.
+//  Copyright © 2018 Bao Doan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Singleton.
+FOUNDATION_EXPORT double SingletonVersionNumber;
+
+//! Project version string for Singleton.
+FOUNDATION_EXPORT const unsigned char SingletonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Singleton/PublicHeader.h>
+
+
